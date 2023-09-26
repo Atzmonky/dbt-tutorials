25ef84ba85e5fa26668b73c43171d3175b4941c0
atzmonky <atzmonky@gmail.com>
2023-09-26 14:50:33 +0200

    added models and  packages

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -5,7 +5,7 @@ source as (
 
 renamed as (
     select 
-        "ID" as id,
+        "ID" as customer_id,
         "FIRST_NAME" as first_name,
         "LAST_NAME" as last_name
     from source
--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -1,12 +1,12 @@
 with 
 source as (
-      select * from {{ source('jaffle_shop', 'orders') }}
+    select * from {{ source('jaffle_shop', 'orders') }}
 ),
 
 renamed as (
     select 
-        "ID" as id,
-        "USER_ID" as user_id,
+        "ID" as order_id,
+        "USER_ID" as customer_id,
         "ORDER_DATE" as order_date,
         "STATUS" as status,
         "_ETL_LOADED_AT" as etl_loaded_at

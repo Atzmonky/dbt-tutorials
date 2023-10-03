d2a205ab2db4742dede425192d2eaceeefc3ec07
atzmonky <atzmonky@gmail.com>
2023-10-03 16:49:52 +0200

    added semantic models

--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -7,7 +7,7 @@ renamed as (
     select 
         "ID" as order_id,
         "USER_ID" as customer_id,
-        "ORDER_DATE" as order_date,
+        date("ORDER_DATE") as order_date,
         "STATUS" as status,
         "_ETL_LOADED_AT" as etl_loaded_at
     from source
